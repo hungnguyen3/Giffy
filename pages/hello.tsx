61e2356b15b082e446750aee8b23992141e2a626
Hung Nguyen <hunglnl01@gmail.com>
2022-03-09 22:07:12 -0800

    tailwind

---
+++ pages/hello.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => {
+	var isItDarkOut = true;
+
+	return <div className="bg-indigo-500">hello there</div>;
+};
+
+export default Home;
