e704b35e897abcd7765d2681015cb0b048f6914e
nathan-n-poon <nathannpoon@gmail.com>
2022-03-23 23:26:04 -0600

    learning REST

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,18 @@
 import express from 'express'
+import bodyParser from 'body-parser'
 
-const app = express()
+const app = express();
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
 
-app.delete('/nathan', (req, res) => {
+function deleteNathan(req: any, res: any) {
+    console.log(req.body);
     return res.send("learning REST");
-})
+} 
+
+app.delete('/nathan/:a&:b', deleteNathan);
 
 app.listen(4000, () => {
 console.log('server running on port 4000');
