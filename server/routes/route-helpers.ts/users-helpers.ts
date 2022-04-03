d09b5b709d7299693bbad2732b2edb349e194659
chiashenglin <bryan2160088@gmail.com>
2022-04-03 01:53:11 -0700

    users/deleteUser endPoint successfully created

--- server/routes/route-helpers.ts/users-helpers.ts
+++ server/routes/route-helpers.ts/users-helpers.ts
@@ -1,17 +1,26 @@
-import { client } from '../../src/index';
+import { client } from "../../src/index";
 
 export const createUser = async (req: any, res: any) => {
-	await client
-		.query(
-			`
+  await client
+    .query(
+      `
         INSERT INTO users ("userName", "firebaseAuthId", "profileImgUrl")
         VALUES ('${req.body.userName}', '${req.body.firebaseAuthId}', '${req.body.profileImgUrl}');
       `
-		)
-		.then(() => res.send('you have successfully created a user'))
-		.catch((e: any) => res.json({ error: e }));
+    )
+    .then(() => res.send("you have successfully created a user"))
+    .catch((e: any) => res.json({ error: e }));
 };
 
 export const deleteUser = async (req: any, res: any) => {
-	return res.send('This is user 1');
+  await client
+    .query(
+      `
+			DELETE FROM users WHERE "userId" = '${req.body.userId}';
+		`
+    )
+    .then(() =>
+      res.send("you have successfully deleted a user uid: " + req.body.userId)
+    )
+    .catch((e: any) => res.json({ error: e }));
 };
