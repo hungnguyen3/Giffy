65922cdb217fccc56900411db2b50271dbe2c6dd
Hung Nguyen <hunglnl01@gmail.com>
2022-04-03 14:48:42 -0700

    nextjs with sass

--- webapp/pages/hello.tsx
+++
@@ -1,9 +0,0 @@
-import type { NextPage } from 'next';
-
-const Home: NextPage = () => {
-	var isItDarkOut = true;
-
-	return <div className="bg-indigo-500">hello there</div>;
-};
-
-export default Home;
