038731b533d788c063fc0b64863e807b1d2d4a41
Hung Nguyen <hunglnl01@gmail.com>
2022-04-07 20:25:41 -0700

    refactor to use async await for users endpoints

--- server/routes/route-helpers.ts/users-helpers.ts
+++ server/routes/route-helpers.ts/users-helpers.ts
@@ -1,98 +1,95 @@
 import { client } from '../../src/index';
 
 export const createUser = async (req: any, res: any) => {
-	await client
-		.query(
+	try {
+		const insertUserRes = await client.query(
 			`
         INSERT INTO users ("userName", "firebaseAuthId", "profileImgUrl")
         VALUES ($1, $2, $3);
       `,
 			[req.body.userName, req.body.firebaseAuthId, req.body.profileImgUrl]
-		)
-		.then(() => res.status(200).send('you have successfully created a user'))
-		.catch((e: any) => {
-			res.status(404).json({ error: e });
-		});
+		);
+
+		if (insertUserRes)
+			return res.status(200).send('you have successfully created a user');
+	} catch (err) {
+		return res.status(404).json({ error: err });
+	}
 };
 
 export const deleteUserById = async (req: any, res: any) => {
-	await client
-		.query(
+	try {
+		const deleteUserRes = await client.query(
 			`
-			DELETE FROM users WHERE "userId" = $1;
+			  DELETE FROM users WHERE "userId" = $1;
 		  `,
 			[req.params.userId]
-		)
-		.then((dbRes: any) => {
-			if (dbRes.rowCount === 0) res.status(404).send('There is no such user');
+		);
 
-			if (dbRes.rowCount === 1) {
-				res
-					.status(200)
-					.send(
-						'you have successfully deleted a user uid: ' + req.params.userId
-					);
-			}
+		if (deleteUserRes.rowCount === 0)
+			return res.status(404).send('There is no such user');
 
-			if (dbRes.rowCount > 1) {
-				res
-					.status(404)
-					.send('error occurred, more than one user with the same id');
-			}
-		})
+		if (deleteUserRes.rowCount === 1)
+			return res
+				.status(200)
+				.send('you have successfully deleted a user uid: ' + req.params.userId);
 
-		.catch((e: any) => res.status(404).json({ error: e }));
+		if (deleteUserRes.rowCount > 1)
+			return res
+				.status(404)
+				.send('error occurred, more than one user with the same id');
+	} catch (err) {
+		return res.status(404).json({ error: err });
+	}
 };
 
 export const getUserById = async (req: any, res: any) => {
-	await client
-		.query(
+	try {
+		const getUserRes = await client.query(
 			`
-		    SELECT* FROM users WHERE "userId" = $1;
-		  `,
+		      SELECT* FROM users WHERE "userId" = $1;
+		    `,
 			[req.params.userId]
-		)
-		.then((dbRes: any) => {
-			if (dbRes.rowCount === 0) res.status(404).send('There is no such user');
+		);
 
-			if (dbRes.rowCount === 1) res.status(200).send(dbRes.rows[0]);
+		if (getUserRes.rowCount === 0)
+			return res.status(404).send('There is no such user');
 
-			if (dbRes.rowCount > 1)
-				res
-					.status(404)
-					.send('error occurred, more than one user with the same id');
-		})
-		.catch((e: any) => res.status(404).json({ error: e }));
+		if (getUserRes.rowCount === 1)
+			return res.status(200).send(getUserRes.rows[0]);
+
+		if (getUserRes.rowCount > 1)
+			return res
+				.status(404)
+				.send('error occurred, more than one user with the same id');
+	} catch (err) {
+		return res.status(404).json({ error: err });
+	}
 };
 
 export const updateUserById = async (req: any, res: any) => {
-	await client
-		.query(
+	try {
+		const updateUserRes = await client.query(
 			`
         UPDATE users
         SET "userName" = COALESCE($1, "userName"), "profileImgUrl"= COALESCE($2, "profileImgUrl")
         WHERE "userId" = $3;
       `,
 			[req.body.userName, req.body.profileImgUrl, req.params.userId]
-		)
-		.then((dbRes: any) => {
-			if (dbRes.rowCount === 0) res.status(404).send('There is no such user');
+		);
+		if (updateUserRes.rowCount === 0)
+			return res.status(404).send('There is no such user');
 
-			if (dbRes.rowCount === 1) {
-				res
-					.status(200)
-					.send(
-						'you have successfully update a user uid: ' + req.params.userId
-					);
-			}
+		if (updateUserRes.rowCount === 1)
+			return res
+				.status(200)
+				.send('you have successfully update a user uid: ' + req.params.userId);
 
-			if (dbRes.rowCount > 1) {
-				res
-					.status(404)
-					.send('error occurred, more than one user with the same id');
-			}
-		})
-		.catch((e: any) => {
-			res.status(404).json({ error: e });
-		});
+		if (updateUserRes.rowCount > 1)
+			return res
+				.status(404)
+				.send('error occurred, more than one user with the same id');
+	} catch (err) {
+		return res.status(404).json({ error: err });
+	}
 };
