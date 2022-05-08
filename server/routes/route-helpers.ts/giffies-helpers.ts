0ddf285d12597b5b0bca9a4b7f65cf2dade9aff8
Hung Nguyen <hunglnl01@gmail.com>
2022-05-07 23:40:11 -0700

    add giffy

--- server/routes/route-helpers.ts/giffies-helpers.ts
+++ server/routes/route-helpers.ts/giffies-helpers.ts
@@ -1,7 +1,26 @@
 import internal from 'stream';
 import { client } from '../../src/index';
 
-export const createGiffy = async (req: any, res: any) => {};
+export const createGiffy = async (req: any, res: any) => {
+	try {
+		const createGiffy = await client.query(
+			`
+      INSERT INTO giffies ("collectionId", "firebaseUrl", "likes")
+      VALUES ($1, $2)
+      RETURNING *;
+      `,
+			[req.body.collectionId, req.body.firebaseUrl, 0]
+		);
+
+		if (createGiffy.rowCount === 1) {
+			return res.status(200).send('you have successfully created a giffy');
+		}
+
+		return res.status(404).json({ error: 'db error' });
+	} catch (err) {
+		return res.status(404).json({ error: err });
+	}
+};
 
 const deleteGiffyUtil = async (collectionId: number, res: any) => {};
 
