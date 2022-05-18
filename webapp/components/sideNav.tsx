de905653cca8be65941220f3ce845b6043be4952
chiashenglin <bryan2160088@gmail.com>
2022-05-18 19:26:43 +0800

    sideBar button style scratch

--- webapp/components/sideNav.tsx
+++ webapp/components/sideNav.tsx
@@ -1,6 +1,7 @@
 import styles from '../styles/Home.module.scss';
 import Link from 'next/link';
 import { useState } from 'react';
+import { AiOutlineArrowLeft, AiOutlineArrowRight } from 'react-icons/ai';
 
 interface SideNavProps {
 	width: string;
@@ -9,23 +10,53 @@ interface SideNavProps {
 const SideNav = (props: SideNavProps) => {
 	const [width, setWidth] = useState(props.width);
 	const closeNav = () => {
-		setWidth('5%');
+		setWidth('10%');
 	};
 
 	const openNav = () => {
 		setWidth(props.width);
 	};
 
+	const [isOpen, setIsOpen] = useState(true);
+
 	return (
-		<div className={styles.sideNav} style={{ width: width }}>
-			<button onClick={closeNav}>X</button>
-			<button onClick={openNav}>O</button>
-			<Link href="/collections">
-				<a>Collections</a>
-			</Link>
-			<Link href="/discovery">
-				<a>Discovery</a>
-			</Link>
+		<div className={styles.something}>
+			<div className={styles.sideNav} style={{ width: width }}>
+				<div>
+					{isOpen ? (
+						<button
+							className={styles.btn}
+							onClick={() => {
+								closeNav();
+								setIsOpen(false);
+							}}
+						>
+							Collapse &nbsp;
+							<AiOutlineArrowLeft></AiOutlineArrowLeft>
+						</button>
+					) : (
+						<button
+							className={styles.btn}
+							onClick={() => {
+								openNav();
+								setIsOpen(true);
+							}}
+						>
+							Open &nbsp;
+							<AiOutlineArrowRight></AiOutlineArrowRight>
+						</button>
+					)}
+				</div>
+
+				<Link href="/collections">
+					<a>Collections</a>
+				</Link>
+
+				<Link href="/discovery">
+					<a>Discovery</a>
+				</Link>
+			</div>
+			<a>hello world</a>
 		</div>
 	);
 };
