d9a74ee2fe80e1c2256a9a04f5c27481e6a3a3ca
chiashenglin <bryan2160088@gmail.com>
2022-05-22 20:39:18 +0800

    add navBar

--- webapp/components/Layout.tsx
+++ webapp/components/Layout.tsx
@@ -1,5 +1,6 @@
 import styles from '../styles/Layout.module.scss';
-import SideNav from './SideNav';
+import SideNav from './sideNav';
+import TopNav from './TopNav';
 
 interface LayoutProps {
 	children: JSX.Element[] | JSX.Element;
@@ -8,7 +9,10 @@ interface LayoutProps {
 const Layout = (props: LayoutProps) => {
 	return (
 		<div className={styles.background}>
-			<SideNav width={'20%'} />
+			<div className={styles.navBars}>
+				<SideNav width={'20%'} />
+				<TopNav />
+			</div>
 			{props.children}
 		</div>
 	);
---
+++ webapp/components/TopNav.tsx
@@ -0,0 +1,41 @@
+import styles from '../styles/TopNav.module.scss';
+import { FcSearch } from 'react-icons/fc';
+import Link from 'next/link';
+
+const TopNav = () => {
+	return (
+		<nav>
+			<div className={styles.TopNav}>
+				<div className={styles.mainTitle}>
+					<Link href="/">
+						<h1>Giffy</h1>
+					</Link>
+				</div>
+
+				<div className={styles.searchContainer}>
+					<form action="/action_page.php">
+						<input type="text" placeholder="Search.." name="search" />
+						<button type="submit">
+							<FcSearch></FcSearch>
+						</button>
+					</form>
+				</div>
+				<div className={styles.menuContainer}>
+					<button type="button" className={styles.userButton}>
+						☰
+					</button>
+					<div className={styles.dropdown}>
+						<ul>
+							<li>Option 1</li>
+							<li>Option 2</li>
+							<li>Option 3</li>
+							<li>Option 4</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</nav>
+	);
+};
+
+export default TopNav;
