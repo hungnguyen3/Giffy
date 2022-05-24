af3a80b46616a85bb24b4f8f281961f349f0c7bf
chiashenglin <bryan2160088@gmail.com>
2022-05-24 16:04:53 +0800

    user button styling

--- webapp/components/TopNav.tsx
+++ webapp/components/TopNav.tsx
@@ -1,8 +1,11 @@
 import styles from '../styles/TopNav.module.scss';
 import { FcSearch } from 'react-icons/fc';
+import { FiUser } from 'react-icons/fi';
 import Link from 'next/link';
+import { useState } from 'react';
 
 const TopNav = () => {
+	const [open, setOpen] = useState(false);
 	return (
 		<nav>
 			<div className={styles.TopNav}>
@@ -16,24 +19,34 @@ const TopNav = () => {
 					<div className={styles.searchContainer}>
 						<form action="/action_page.php">
 							<input type="text" placeholder="Search.." name="search" />
-							<button type="submit">
+							<button type="submit" className={styles.button}>
 								<FcSearch></FcSearch>
 							</button>
 						</form>
 					</div>
 				</div>
 				<div className={styles.menuContainer}>
-					<button type="button" className={styles.userButton}>
-						☰
+					<button
+						type="button"
+						className={styles.userButton}
+						onClick={() => {
+							setOpen(!open);
+						}}
+					>
+						<FiUser />
 					</button>
-					<div className={styles.dropdown}>
-						<ul>
-							<li>Option 1</li>
-							<li>Option 2</li>
-							<li>Option 3</li>
-							<li>Option 4</li>
-						</ul>
-					</div>
+					{open ? (
+						<div className={styles.dropdown}>
+							<ul>
+								<li>Option 1</li>
+								<li>Option 2</li>
+								<li>Option 3</li>
+								<li>Option 4</li>
+							</ul>
+						</div>
+					) : (
+						<div></div>
+					)}
 				</div>
 			</div>
 		</nav>
