a7915295c467c704b4e32955ce6224c466e911bb
Shaun <sbshaun7@gmail.com>
2022-06-28 16:03:07 -0700

    deleted comments

--- webapp/components/Firebase/FirebaseInit.ts
+++ webapp/components/Firebase/FirebaseInit.ts
@@ -1,4 +1,3 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import {
 	getAuth,
@@ -7,11 +6,7 @@ import {
 	Auth,
 	AuthProvider,
 } from 'firebase/auth';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
 	apiKey: process.env.NEXT_PUBLIC_APIKEY,
 	authDomain: process.env.NEXT_PUBLIC_AUTHDOMAIN,
@@ -22,10 +17,8 @@ const firebaseConfig = {
 	measurementId: process.env.NEXT_PUBLIC_MEASUREMENTID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Initialize Firebase Authentication and get a reference to the service
 const auth = getAuth(app);
 
 const provider = new GoogleAuthProvider();
@@ -33,21 +26,15 @@ const provider = new GoogleAuthProvider();
 export const googleSignIn = () => {
 	signInWithPopup(auth, provider)
 		.then(result => {
-			// This gives you a Google Access Token. You can use it to access the Google API.
 			const credential = GoogleAuthProvider.credentialFromResult(result);
 			const token = credential?.accessToken;
-			// The signed-in user info.
 			const user = result.user;
 			// ...
 		})
 		.catch(error => {
-			// Handle Errors here.
 			const errorCode = error.code;
 			const errorMessage = error.message;
-			// The email of the user's account used.
 			const email = error.customData.email;
-			// The AuthCredential type that was used.
 			const credential = GoogleAuthProvider.credentialFromError(error);
-			// ...
 		});
 };
